613ee48288ce2a023c0ef1c341644ef5bda7906a
smk5g5 <skhan23@wustl.edu>
2022-01-28 10:35:03 -0600

    Added normal and malignant gene lists in a format that would work for scsorter
    Added marker genes from multiple studies to the list!
    
    essential_inputs/Ivy_markers_top25_list.rds
    essential_inputs/Ivy_markers_top50_list.rds
    essential_inputs/Li_etal_list.rds
    essential_inputs/Nowaski_DevelopingBrain_list.rds
    essential_inputs/Wang_CancerCell_list.rds
    essential_inputs/Wang_SingleCell_CancerDiscovery_list.rds

---
+++ scripts/useful_snippets.R
@@ -0,0 +1,65 @@
+df_to_marker_list <- function(marker_df,cluster_col,gene_col){
+  sub_marker_list <- list()
+  for(i in unique(marker_df[[cluster_col]])){
+    subdf <- marker_df[marker_df[[cluster_col]]==i,]
+    sub_marker_list[[i]]  <- subdf[[gene_col]]
+  }
+return(sub_marker_list)
+  }
+
+# get_topn_bycol <- function(mydf,group_col,arrange_col,slice_no){
+# topn_mydf <- mydf %>%
+#   group_by_at(group_col) %>%
+#   arrange(desc(.dots=arrange_col)) %>%
+#   slice(1:slice_no)
+#   return(topn_mydf)
+# }
+
+Nowaski_DevelopingBrain <- read.table('/storage1/fs1/allegra.petti/Active/10xGenomics/key.gene.lists/brain_cell_markers/Nowaski_DevelopingBrain.txt',header=T)
+
+Nowaski_DevelopingBrain_list <- df_to_marker_list(Nowaski_DevelopingBrain,cluster_col='List',gene_col='gene_name')
+
+Li_etal <- read.table('/storage1/fs1/allegra.petti/Active/10xGenomics/key.gene.lists/brain_cell_markers/Li.txt',header=T,sep="\t")
+
+Li_etal_list <- df_to_marker_list(Li_etal,cluster_col='List',gene_col='gene_name')
+
+Ivy_markers <- read.table('/storage1/fs1/allegra.petti/Active/10xGenomics/key.gene.lists/brain_cell_markers/Ivy_markers.txt',header=T)
+
+Ivy_markers_top25 <- Ivy_markers %>%
+  group_by(List) %>%
+  arrange(desc(minFoldChange)) %>%
+  slice(1:25)
+
+Ivy_markers_top25_list <- df_to_marker_list(Ivy_markers_top25,cluster_col='List',gene_col='gene_name')
+
+Ivy_markers_top50 <- Ivy_markers %>%
+  group_by(List) %>%
+  arrange(desc(minFoldChange)) %>%
+  slice(1:50)
+
+Ivy_markers_top50_list <- df_to_marker_list(Ivy_markers_top50,cluster_col='List',gene_col='gene_name')
+
+Wang_CancerCell <- read.table('/storage1/fs1/allegra.petti/Active/10xGenomics/key.gene.lists/brain_cell_markers/Wang_CancerCell.txt',header=T)
+
+Wang_SingleCell_CancerDiscovery <- read.table('/storage1/fs1/allegra.petti/Active/10xGenomics/key.gene.lists/brain_cell_markers/Wang_SingleCell_CancerDiscovery2019.txt',header=T)
+
+Wang_CancerCell_list <- df_to_marker_list(Wang_CancerCell,cluster_col='List',gene_col='gene_name')
+
+Wang_SingleCell_CancerDiscovery_list <- df_to_marker_list(Wang_SingleCell_CancerDiscovery,cluster_col='List',gene_col='gene_name')
+
+saveRDS(Ivy_markers_top25_list,'./essential_inputs/Ivy_markers_top25_list.rds')
+saveRDS(Ivy_markers_top50_list,'./essential_inputs/Ivy_markers_top50_list.rds')
+saveRDS(Li_etal_list,'./essential_inputs/Li_etal_list.rds')
+saveRDS(Wang_CancerCell_list,'./essential_inputs/Wang_CancerCell_list.rds')
+saveRDS(Wang_SingleCell_CancerDiscovery_list,'./essential_inputs/Wang_SingleCell_CancerDiscovery_list.rds')
+saveRDS(Nowaski_DevelopingBrain_list,'./essential_inputs/Nowaski_DevelopingBrain_list.rds')
+
+
+#/rdcw/fs1/allegra.petti/Active/10xGenomics/key.gene.lists/brain_cell_markers/Ivy_markers.txt
+
+#/rdcw/fs1/allegra.petti/Active/10xGenomics/key.gene.lists/brain_cell_markers/Ivy_markers.txt
+
+#
+
+#/rdcw/fs1/allegra.petti/Active/10xGenomics/key.gene.lists/brain_cell_markers/Wang_SingleCell_CancerDiscovery2019.txt
+
