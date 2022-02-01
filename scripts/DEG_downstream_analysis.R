20ae59a7ad34bdff9d48b343024c0c59f2becd52
smk5g5 <skhan23@wustl.edu>
2022-01-31 18:42:06 -0600

    adjusting for size

--- scripts/DEG_downstream_analysis.R
+++ scripts/DEG_downstream_analysis.R
@@ -67,8 +67,8 @@ myhtmp_phtmp <- ComplexHeatmap::pheatmap(clust.means.norm,cluster_rows = T,clust
                                           border_color='black', scale="none",breaks=colors,color=my.palette,fontsize=30,fontsize_row=16,fontsize_col=30,
                                           cellwidth = ncol(clust.means.norm)*unit(10, "mm"), cellheight = nrow(clust.means.norm)*unit(5, "mm"))
 
-fig_ht <- nrow(clust.means.norm)*unit(6, "mm")
-fig_wd <- 2 * ncol(clust.means.norm)*unit(13, "mm")
+fig_ht <- nrow(clust.means.norm)*unit(5, "mm")
+fig_wd <- 2 * ncol(clust.means.norm)*unit(5, "mm")
 
 png(filename=sprintf("%s_RNAassay.%s.aggregatedhtmap.%s.png",prefix,clustering,date), units="cm", res=300, height=fig_ht, width=fig_wd);
 print(myhtmp_phtmp)
