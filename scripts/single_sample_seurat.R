c31043330c010b77f4543fbc4400bc3a12cd9500
Saad khan <skhan41@mgh.harvard.edu>
2023-06-12 16:11:43 -0500

    Update single_sample_seurat.R
    Update single_sample_seurat.R

--- scripts/single_sample_seurat.R
+++ scripts/single_sample_seurat.R
@@ -180,28 +180,28 @@ vln <- VlnPlot(object = scrna_GEX, features = "nFeature_RNA", pt.size=0, group.b
 print(vln)
 dev.off()
 
-get_significant_pcs <- function(scrna_GEX) {
-  control='Cycling'
-  if(length(unique(scrna_GEX$orig.ident))==1){
-    nPC=20 
-  }else{
-    nPC=50
-  }
-  # print('Does the error happen inside get_significant_pcs?')
-  scrna_GEX <- RunPCA(scrna_GEX, npcs = nPC, verbose = FALSE)
-  scrna_GEX <- JackStraw(object = scrna_GEX, num.replicate = 100, dims=nPC)
-  scrna_GEX <- ScoreJackStraw(object = scrna_GEX, dims = 1:nPC)
-  jpeg(sprintf("PCA.jackstraw.%s.%s.jpg", control, date), width = 10, height = 6, units="in", res=300);
-  js <- JackStrawPlot(object = scrna_GEX, dims = 1:nPC)
-  print(js);
-  dev.off();
-  pc.pval <- scrna_GEX@reductions$pca@jackstraw@overall.p.values
-  print(pc.pval);
-  nPC=length( pc.pval[,'Score'][pc.pval[,'Score'] <= 0.05]) 
-  # print('No the error does not happen inside get_significant_pcs!')
-  #redefine nPCs based on number of significant prinicipal components in jackstraw plot
-  return(nPC)
-}
+# get_significant_pcs <- function(scrna_GEX) {
+#   control='Cycling'
+#   if(length(unique(scrna_GEX$orig.ident))==1){
+#     nPC=20 
+#   }else{
+#     nPC=50
+#   }
+#   # print('Does the error happen inside get_significant_pcs?')
+#   scrna_GEX <- RunPCA(scrna_GEX, npcs = nPC, verbose = FALSE)
+#   scrna_GEX <- JackStraw(object = scrna_GEX, num.replicate = 100, dims=nPC)
+#   scrna_GEX <- ScoreJackStraw(object = scrna_GEX, dims = 1:nPC)
+#   jpeg(sprintf("PCA.jackstraw.%s.%s.jpg", control, date), width = 10, height = 6, units="in", res=300);
+#   js <- JackStrawPlot(object = scrna_GEX, dims = 1:nPC)
+#   print(js);
+#   dev.off();
+#   pc.pval <- scrna_GEX@reductions$pca@jackstraw@overall.p.values
+#   print(pc.pval);
+#   nPC=length( pc.pval[,'Score'][pc.pval[,'Score'] <= 0.05]) 
+#   # print('No the error does not happen inside get_significant_pcs!')
+#   #redefine nPCs based on number of significant prinicipal components in jackstraw plot
+#   return(nPC)
+# } ##this is used in another script
 
 scrna_GEX <- NormalizeData(object = scrna_GEX, normalization.method = "LogNormalize", scale.factor = 1e6); # 1e6 is new as of 1/8/20
 
@@ -214,91 +214,91 @@ vg <- VariableFeaturePlot(scrna_GEX)
 print(vg);
 dev.off()
 
-nPC <- get_significant_pcs(scrna_GEX)
-scrna_GEX <- RunPCA(scrna_GEX, npcs = nPC, verbose = FALSE)
+# nPC <- get_significant_pcs(scrna_GEX)
+# scrna_GEX <- RunPCA(scrna_GEX, npcs = nPC, verbose = FALSE)
 
-scrna_GEX <- RunUMAP(object = scrna_GEX, reduction = "pca", dims = 1:nPC)
-scrna_GEX <- RunTSNE(object = scrna_GEX, reduction = "pca", dims = 1:nPC)
-scrna_GEX <- JackStraw(object = scrna_GEX, num.replicate = 100, dims=nPC)
-scrna_GEX <- ScoreJackStraw(object = scrna_GEX, dims = 1:nPC)
-jpeg(sprintf("PCA.jackstraw.%s.%s.%s.jpg",sample,control, date), width = 10, height = 6, units="in", res=300);
-js <- JackStrawPlot(object = scrna_GEX, dims = 1:nPC)
-print(js);
-dev.off();
+# scrna_GEX <- RunUMAP(object = scrna_GEX, reduction = "pca", dims = 1:nPC)
+# scrna_GEX <- RunTSNE(object = scrna_GEX, reduction = "pca", dims = 1:nPC)
+# scrna_GEX <- JackStraw(object = scrna_GEX, num.replicate = 100, dims=nPC)
+# scrna_GEX <- ScoreJackStraw(object = scrna_GEX, dims = 1:nPC)
+# jpeg(sprintf("PCA.jackstraw.%s.%s.%s.jpg",sample,control, date), width = 10, height = 6, units="in", res=300);
+# js <- JackStrawPlot(object = scrna_GEX, dims = 1:nPC)
+# print(js);
+# dev.off();
 
-jpeg(sprintf("UMAP.%s.%s.%s.jpg",sample,control, date), width = 10, height = 8, units="in", res=300);
-p2 <- DimPlot(object = scrna_GEX, reduction = "umap", group.by = "Sample", pt.size=0.1)
-print(p2);
-dev.off();
+# jpeg(sprintf("UMAP.%s.%s.%s.jpg",sample,control, date), width = 10, height = 8, units="in", res=300);
+# p2 <- DimPlot(object = scrna_GEX, reduction = "umap", group.by = "Sample", pt.size=0.1)
+# print(p2);
+# dev.off();
 
-print ("VizDimLoadings Running...");
-jpeg(sprintf("VizDimLoadings.%s.%s.%s.jpg",sample,control, date), width = 8, height = 30, units="in", res=300);
-vdl <- VizDimLoadings(object = scrna_GEX, dims = 1:3)
-print(vdl);
-dev.off();
+# print ("VizDimLoadings Running...");
+# jpeg(sprintf("VizDimLoadings.%s.%s.%s.jpg",sample,control, date), width = 8, height = 30, units="in", res=300);
+# vdl <- VizDimLoadings(object = scrna_GEX, dims = 1:3)
+# print(vdl);
+# dev.off();
 
-print ("ProjectDim Running...");
-scrna_GEX <- ProjectDim(object = scrna_GEX)
+# print ("ProjectDim Running...");
+# scrna_GEX <- ProjectDim(object = scrna_GEX)
 
-# saveRDS(scrna_GEX, file = sprintf("%s.SCT.PCA.UMAP.TSNE.%s.rds",control, date))
+# # saveRDS(scrna_GEX, file = sprintf("%s.SCT.PCA.UMAP.TSNE.%s.rds",control, date))
 
-print ("DimHeatmap Running...");
-jpeg(sprintf("PCA.heatmap.top.%s.%s.%s.jpg",sample,control, date), width = 8.5, height = 11, units="in", res=300);
-hm <- DimHeatmap(object = scrna_GEX, dims = 1, cells = 500, balanced = TRUE);
-print(hm);
-dev.off();
+# print ("DimHeatmap Running...");
+# jpeg(sprintf("PCA.heatmap.top.%s.%s.%s.jpg",sample,control, date), width = 8.5, height = 11, units="in", res=300);
+# hm <- DimHeatmap(object = scrna_GEX, dims = 1, cells = 500, balanced = TRUE);
+# print(hm);
+# dev.off();
 
-jpeg(sprintf("PCA.heatmap.multi.%s.%s.%s.jpg",sample,control, date), width = 8.5, height = 24, units="in", res=300);
-hm.multi <- DimHeatmap(object = scrna_GEX, dims = 1:10, cells = 500, balanced = TRUE);
-print(hm.multi);
-dev.off();
+# jpeg(sprintf("PCA.heatmap.multi.%s.%s.%s.jpg",sample,control, date), width = 8.5, height = 24, units="in", res=300);
+# hm.multi <- DimHeatmap(object = scrna_GEX, dims = 1:10, cells = 500, balanced = TRUE);
+# print(hm.multi);
+# dev.off();
 
-scrna_GEX <- FindNeighbors(scrna_GEX, reduction = "pca", dims = 1:nPC)
-scrna_GEX <- FindClusters(scrna_GEX, resolution = 0.5)
-scrna_GEX[[sprintf("ClusterNames_%.1f_%dPC",0.5, nPC)]] <- Idents(scrna_GEX)
-scrna_GEX <- FindClusters(scrna_GEX, resolution = 0.7)
-scrna_GEX[[sprintf("ClusterNames_%.1f_%dPC",0.7, nPC)]] <- Idents(scrna_GEX)
-scrna_GEX <- FindClusters(scrna_GEX, resolution = 0.9)
-scrna_GEX[[sprintf("ClusterNames_%.1f_%dPC",0.9, nPC)]] <- Idents(scrna_GEX)
+# scrna_GEX <- FindNeighbors(scrna_GEX, reduction = "pca", dims = 1:nPC)
+# scrna_GEX <- FindClusters(scrna_GEX, resolution = 0.5)
+# scrna_GEX[[sprintf("ClusterNames_%.1f_%dPC",0.5, nPC)]] <- Idents(scrna_GEX)
+# scrna_GEX <- FindClusters(scrna_GEX, resolution = 0.7)
+# scrna_GEX[[sprintf("ClusterNames_%.1f_%dPC",0.7, nPC)]] <- Idents(scrna_GEX)
+# scrna_GEX <- FindClusters(scrna_GEX, resolution = 0.9)
+# scrna_GEX[[sprintf("ClusterNames_%.1f_%dPC",0.9, nPC)]] <- Idents(scrna_GEX)
 
-scrna_GEX <- FindClusters(scrna_GEX, resolution = 1.2)
-scrna_GEX[[sprintf("ClusterNames_%.1f_%dPC",1.2, nPC)]] <- Idents(scrna_GEX)
-Idents(scrna_GEX) <- sprintf("ClusterNames_%.1f_%dPC",1.2, nPC)
+# scrna_GEX <- FindClusters(scrna_GEX, resolution = 1.2)
+# scrna_GEX[[sprintf("ClusterNames_%.1f_%dPC",1.2, nPC)]] <- Idents(scrna_GEX)
+# Idents(scrna_GEX) <- sprintf("ClusterNames_%.1f_%dPC",1.2, nPC)
 
 
-n.graph = length(unique(Idents(scrna_GEX)))
-print(n.graph)
-rainbow.colors = rainbow(n.graph, s=0.6, v=0.9);
-names(rainbow.colors) <- sort(unique(Idents(scrna_GEX)))
-print(rainbow.colors)
-print(length(rainbow.colors))
+# n.graph = length(unique(Idents(scrna_GEX)))
+# print(n.graph)
+# rainbow.colors = rainbow(n.graph, s=0.6, v=0.9);
+# names(rainbow.colors) <- sort(unique(Idents(scrna_GEX)))
+# print(rainbow.colors)
+# print(length(rainbow.colors))
 
-jpeg(sprintf("UMAP.clusters.%s.%d.%.1f.%s.jpg",sample,nPC, 1.2, date), width = 10, height = 8, units="in", res=300);
-p2 <- DimPlot(object = scrna_GEX, reduction = "umap", group.by = sprintf("ClusterNames_%.1f_%dPC",0.7, nPC), cols = rainbow.colors, pt.size=0.1) + theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-print(p2);
-dev.off();
+# jpeg(sprintf("UMAP.clusters.%s.%d.%.1f.%s.jpg",sample,nPC, 1.2, date), width = 10, height = 8, units="in", res=300);
+# p2 <- DimPlot(object = scrna_GEX, reduction = "umap", group.by = sprintf("ClusterNames_%.1f_%dPC",0.7, nPC), cols = rainbow.colors, pt.size=0.1) + theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# print(p2);
+# dev.off();
 
-jpeg(sprintf("UMAP.clusters.labeled.%s.%d.%.1f.%s.jpg",sample,nPC, 1.2, date), width = 10, height = 8, units="in", res=300);
-p2 <- DimPlot(object = scrna_GEX, reduction = "umap", group.by = sprintf("ClusterNames_%.1f_%dPC",0.7, nPC), cols = rainbow.colors, pt.size=0.1, label=TRUE,label.size = 5) + theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-print(p2);
-dev.off();
+# jpeg(sprintf("UMAP.clusters.labeled.%s.%d.%.1f.%s.jpg",sample,nPC, 1.2, date), width = 10, height = 8, units="in", res=300);
+# p2 <- DimPlot(object = scrna_GEX, reduction = "umap", group.by = sprintf("ClusterNames_%.1f_%dPC",0.7, nPC), cols = rainbow.colors, pt.size=0.1, label=TRUE,label.size = 5) + theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# print(p2);
+# dev.off();
 
-print ("color UMAP by Principal Components");
-jpeg(sprintf("UMAP.%d.%.1f.colorby.PCs.%s.%s.jpg",sample,nPC, 1.2, date), width = 12, height = 6, units="in", res=100);
-redblue=c("blue","gray","red");
-fp1 <- FeaturePlot(object = scrna_GEX, features = 'PC_1', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp2 <- FeaturePlot(object = scrna_GEX, features = 'PC_2', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp3 <- FeaturePlot(object = scrna_GEX, features = 'PC_3', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp4 <- FeaturePlot(object = scrna_GEX, features = 'PC_4', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp5 <- FeaturePlot(object = scrna_GEX, features = 'PC_5', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp6 <- FeaturePlot(object = scrna_GEX, features = 'PC_6', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp7 <- FeaturePlot(object = scrna_GEX, features = 'PC_7', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp8 <- FeaturePlot(object = scrna_GEX, features = 'PC_8', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp9 <- FeaturePlot(object = scrna_GEX, features = 'PC_9', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-fp10 <- FeaturePlot(object = scrna_GEX, features = 'PC_10', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
-print(plot_grid(fp1, fp2, fp3, fp4, fp5, fp6, fp7, fp8, fp9, fp10));
+# print ("color UMAP by Principal Components");
+# jpeg(sprintf("UMAP.%d.%.1f.colorby.PCs.%s.%s.jpg",sample,nPC, 1.2, date), width = 12, height = 6, units="in", res=100);
+# redblue=c("blue","gray","red");
+# fp1 <- FeaturePlot(object = scrna_GEX, features = 'PC_1', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp2 <- FeaturePlot(object = scrna_GEX, features = 'PC_2', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp3 <- FeaturePlot(object = scrna_GEX, features = 'PC_3', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp4 <- FeaturePlot(object = scrna_GEX, features = 'PC_4', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp5 <- FeaturePlot(object = scrna_GEX, features = 'PC_5', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp6 <- FeaturePlot(object = scrna_GEX, features = 'PC_6', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp7 <- FeaturePlot(object = scrna_GEX, features = 'PC_7', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp8 <- FeaturePlot(object = scrna_GEX, features = 'PC_8', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp9 <- FeaturePlot(object = scrna_GEX, features = 'PC_9', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# fp10 <- FeaturePlot(object = scrna_GEX, features = 'PC_10', cols=redblue, pt.size=0.1, reduction = "umap")+ theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank());
+# print(plot_grid(fp1, fp2, fp3, fp4, fp5, fp6, fp7, fp8, fp9, fp10));
 # print(plot_grid(fp1, fp2, fp3, fp4, fp5, fp6, fp7, fp8, fp9));
-dev.off();
+# dev.off();
 
 #only for testing purposes
 # set.seed(100)
