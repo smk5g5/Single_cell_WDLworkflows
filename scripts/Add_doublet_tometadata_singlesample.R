ddd6b29fa05e384ff6234500a3adcd1fd586525d
Saad khan <skhan41@mgh.harvard.edu>
2023-06-13 11:54:17 -0500

    New version (Doublet collection) has Scrublet installation inbuilt old version scrublet installation non-trivial
    Removed scrublet from the call (in New version of Doublet collection scrublet installation is non-trivial in old version its not)
    
    The new version and new release with WDL 1.1 will be available with new release of the same.

--- scripts/Add_doublet_tometadata_singlesample.R
+++ scripts/Add_doublet_tometadata_singlesample.R
@@ -39,7 +39,7 @@ doublet_object <- readRDS(doublet_file)
 
 add_doublet_predictions_to_seurat_singlesample <- function(seurat_object,doublet_object){
   # doublet_object[['doublet_results']] <- FindDoublets(score.list=doublet_object$doublet_scores,rate=0.08)
-  for(i in c("cxds","bcds","hybrid","scDblFinder","Scrublet","DoubletFinder")){
+  for(i in c("cxds","bcds","hybrid","scDblFinder","DoubletFinder")){
     sel_index <- doublet_object[['doublet_results']][[i]]
     doublet_cells <- doublet_object$cellnames[sel_index]
     non_doublet_cells <- setdiff(doublet_object$cellnames,doublet_cells)
